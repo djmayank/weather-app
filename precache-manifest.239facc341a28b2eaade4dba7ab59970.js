d9bc504c5962f374acbaa52402823b440b612019
mayank <dj.mayankkhanna@gmail.com>
2020-12-13 18:53:48 +0530

    Updates

---
+++ precache-manifest.239facc341a28b2eaade4dba7ab59970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43f7aecbc563ffc5f0771af7152c641",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "19a027f207dee3d96c31",
+    "url": "/weather-app/static/css/main.5c524dda.chunk.css"
+  },
+  {
+    "revision": "14745ac27569bb437a82",
+    "url": "/weather-app/static/js/2.b2a9edbe.chunk.js"
+  },
+  {
+    "revision": "19a027f207dee3d96c31",
+    "url": "/weather-app/static/js/main.a5dc2c43.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "c9d3051bb9da841267b1ec99c7c5fb23",
+    "url": "/weather-app/static/media/hill4.c9d3051b.svg"
+  }
+]);
